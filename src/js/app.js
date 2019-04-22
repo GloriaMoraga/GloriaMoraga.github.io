cc2b1d69d099acaf60cae0b97d97403c65e8c324
GloriaMoraga <silmeriel@gail.com>
2019-04-22 17:38:08 -0400

    formulario listo

--- src/js/app.js
+++ src/js/app.js
@@ -7,5 +7,18 @@
      
     const toltip = document.querySelectorAll('.tooltipped');
     M.Tooltip.init(toltip, {});
+
+
+    document.getElementById('btnSend').addEventListener('click', ()=>{
+        document.getElementById('mensaje-exitoso').style.display='block';
+        document.getElementById('gform').style.display='none';
    
+              
+        })
+
+    
+
+
+   
+
   
